6879a801e4af5ab713d241463800829d8bbd7ab1
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-07 08:10:25 -0700

    Attaches dice role function to button trigger on html page

--- 1_Dice/3_dice_returnting_values_start/dice.js
+++ 1_Dice/3_dice_returnting_values_start/dice.js
@@ -3,13 +3,6 @@ var dice = {
   roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     console.log(randomNumber);
-  }
-}
-
-var dice10 = {
-  sides: 10,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    console.log(randomNumber);
+    return randomNumber;
   }
 }
